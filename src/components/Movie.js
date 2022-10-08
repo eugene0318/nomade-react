e6bf8572cce9e5624b4c25c667228d90e5333e80
eugene0318 <nttott@gmail.com>
2022-10-09 03:43:55 +0900

    test

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
     <div>
       <img src={coverImg} alt="img" />
       <Link to={`/movie/${id}`}>{title}</Link>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
